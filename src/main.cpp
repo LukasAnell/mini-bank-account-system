25ab8fc01df3bcb4f46edc32cda5a96d5f2529a1
LukasAnell <lukasmanell@gmail.com>
2026-05-22 13:29:40 -0700

    Added support for multiple accounts, added an option to switch accounts, improved bad input handling, swapped out chained if statements in favor of switch statements for menu options

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
+#include <cstddef>
 #include <iomanip>
 #include <iostream>
 #include <string>
 #include <format>
+#include <vector>
 
 double deposit(double balance, double amount) {
     if (amount == 0) {
@@ -36,39 +38,73 @@ double withdraw(double balance, double amount) {
 }
 
 int main() {
-    std::string transactionHistory { };
+    std::vector<std::string> transactionHistories { };
 
-    std::string holderName { };
-    double balance { };
+    std::vector<std::string> names { };
+    std::vector<double> balances { };
 
-    std::cout << "Enter account holder name: ";
-    std::getline(std::cin, holderName);
+    int numAccounts { };
+    std::cout << "How many accounts? ";
+    std::cin >> numAccounts;
 
-    std::cout << "Enter opening balance: ";
-    std::cin >> balance;
+    for (int i { 1 }; i <= numAccounts; ++i) {
+        std::cout << '\n';
+
+        std::string name { };
+        double balance { };
+
+        std::cout << "Account " << i << " name: ";
+        std::getline(std::cin >> std::ws, name);
+        names.push_back(name);
+
+        // std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        std::cout << "Account " << i << " opening balance: ";
+        std::cin >> balance;
+        balances.push_back(balance);
+
+        transactionHistories.push_back("");
+    }
 
     std::cout << '\n';
 
-    // opening balance was negative
-    if (balance < 0) {
-        std::cout << "Sorry, your opening balance cannot be negative.\n";
-        return 0;
+    std::cout << "--- Accounts ---\n";
+    for (size_t i { }; i < names.size(); ++i) {
+        std::string name { names.at(i) };
+        double balance { balances.at(i) };
+
+        if (balance < 0) {
+            std::cout <<"Your opening balance cannot be negative.\n";
+            return 0;
+        }
+
+        std::string accountType { };
+        if (balance > 1000) {
+            accountType = "Premium";
+        } else {
+            accountType = "Standard";
+        }
+
+        std::cout << (i + 1)  << ". " << name << " $" << std::fixed << std::setprecision(2) << balance << " [" << accountType << "]\n";
     }
 
-    // account is premium vs standard
-    if (balance > 1000) {
-        std::cout << "[Premium Account]\n";
-    } else {
-        std::cout << "[Standard Account]\n";
-    }
+    std::cout << '\n';
 
-    std::cout << "Welcome, " << holderName << "!\n";
-    std::cout << "Current balance: $" << std::fixed << std::setprecision(2) << balance << '\n';
+    size_t selectedAccountIndex { 0 };
+    std::string name = names.at(selectedAccountIndex);
+    double balance = balances.at(selectedAccountIndex);
+
+    bool menuLoop { true };
 
     // menu loop
-    while (true) {
+    while (menuLoop) {
         std::cout << '\n';
+
+        std::cout << "Active account: " << name << '\n';
+
         std::cout << "--- Menu ---\n";
+
+        std::cout << "0. Switch Account\n";
         std::cout << "1. Deposit\n";
         std::cout << "2. Withdraw\n";
         std::cout << "3. View Balance\n";
@@ -78,45 +114,112 @@ int main() {
         std::cout << "Choice: ";
         std::cin >> choice;
 
-        if (choice == 1) {
-            double amount { };
-            std::cout << "Amount: ";
-            std::cin >> amount;
+        switch (choice) {
+            case 0: {
+                int newIndex { };
+                std::cout << "Select account (1-" << names.size() << "): ";
+                std::cin >> newIndex;
 
-            double newBalance { deposit(balance, amount) };
+                newIndex--;
 
-            if (newBalance != balance) {
-                transactionHistory += ("Deposit $" + std::format("{:.2f}", amount) + "\n");
+                if (newIndex < 0 || static_cast<size_t>(newIndex) > names.size()) {
+                    std::cout << "Please select a number between 1-" << names.size() << ".\n";
 
-                std::cout << "Deposited $" << std::fixed << std::setprecision(2) << amount << ". New balance: $" << newBalance << '\n';
+                    continue;
+                }
+
+                selectedAccountIndex = static_cast<size_t>(newIndex);
+
+                name = names.at(selectedAccountIndex);
+                balance = balances.at(selectedAccountIndex);
+
+                std::cout << "Switched to " << name << ".\n";
+
+                break;
             }
+            case 1: {
+                double amount { };
+                std::cout << "Amount: ";
+                std::cin >> amount;
 
-            balance = newBalance;
+                double newBalance { deposit(balance, amount) };
 
-        } else if (choice == 2) {
-            double amount { };
-            std::cout << "Amount: ";
-            std::cin >> amount;
+                if (newBalance != balance) {
+                    transactionHistories.at(selectedAccountIndex) += ("Deposit $" + std::format("{:.2f}", amount) + "\n");
 
-            double newBalance { withdraw(balance, amount) };
+                    std::cout << "Deposited $" << std::fixed << std::setprecision(2) << amount << ". New balance: $" << newBalance << '\n';
+                }
 
-            if (newBalance != balance) {
-                transactionHistory += ("Withdraw $" + std::format("{:.2f}", amount) + "\n");
+                balance = newBalance;
 
-                std::cout << "Withdrawn $" << std::fixed << std::setprecision(2) << amount << ". New balance: $" << newBalance << '\n';
+                break;
             }
+            case 2: {
+                double amount { };
+                std::cout << "Amount: ";
+                std::cin >> amount;
 
-            balance = newBalance;
-        } else if (choice == 3) {
-            std::cout << "Transaction History: \n";
-            std::cout << transactionHistory << '\n';
+                double newBalance { withdraw(balance, amount) };
 
-            std::cout << "Your account balance is: $" << std::fixed << std::setprecision(2) << balance << '\n';
-        } else if(choice == 4) {
-            std::cout << "Goodbye, " << holderName << "!\n";
+                if (newBalance != balance) {
+                    transactionHistories.at(selectedAccountIndex) += ("Withdraw $" + std::format("{:.2f}", amount) + "\n");
+
+                    std::cout << "Withdrawn $" << std::fixed << std::setprecision(2) << amount << ". New balance: $" << newBalance << '\n';
+                }
+
+                balance = newBalance;
+
+                break;
+            }
+            case 3: {
+                std::cout << "Transaction History: \n";
+
+                if (transactionHistories.at(selectedAccountIndex).empty()) {
+                    std::cout << "No transaction history recorded yet.\n";
+                } else {
+                    std::cout << transactionHistories.at(selectedAccountIndex) << '\n';
+                }
+
+                std::cout << "Your account balance is: $" << std::fixed << std::setprecision(2) << balance << '\n';
+
+                break;
+            }
+            case 4: {
+                std::cout << "--- Accounts ---\n";
+                for (size_t i { }; i < names.size(); ++i) {
+                    std::string n { names.at(i) };
+                    double b { balances.at(i) };
+
+                    if (b < 0) {
+                        std::cout <<"Your opening balance cannot be negative.\n";
+                        return 0;
+                    }
+
+                    std::string accountType { };
+                    if (b > 1000) {
+                        accountType = "Premium";
+                    } else {
+                        accountType = "Standard";
+                    }
+
+                    std::cout << (i + 1)  << ". " << n << " $" << std::fixed << std::setprecision(2) << b << " [" << accountType << "]\n";
+                }
+
+                std::cout << "Goodbye!\n";
+
+                menuLoop = false;
+
+                break;
+            }
+            default: {
+                std::cout << "Please input a number 0-4.\n";
+
+                break;
+            }
+        }
+
+        if (!menuLoop) {
             break;
-        } else {
-            std::cout << "Please input a number 1-4.\n";
         }
     }
 
